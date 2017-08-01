6d47bf385d19ca82933d9c1d337b3db1249ffaf4
Pavlo <p.bazyliuk@gmail.com>
2017-08-02 00:17:16 +0300

    added some css fixes & light animation for image slider

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,17 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import styles from "./Header.scss";
 
 class Header extends React.Component {
   render() {
     return (
       <header className={styles["main-header"]}>
-        <div className={styles["logo-container"]}>Eleks</div>
+        <div className={styles["logo-container"]}>
+          <Link className={styles["logo-link"]} to="/">
+            Eleks
+          </Link>
+        </div>
+
         <div className={styles["project-description"]}>Mentoring platform</div>
       </header>
     );
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,115 +1,116 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from '../Home/Home';
-import LoginContainer from '../../containers/LoginContainer';
-import styles from './Main.scss';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "../Home/Home";
+import LoginContainer from "../../containers/LoginContainer";
+import styles from "./Main.scss";
 
 var sliderBtnActiveStyle = {
-	backgroundColor: '#fff'
+  transition: "all 0.4s ease-out",
+  backgroundColor: "#fff"
 };
 
 var imgOne = {
-	background:
-		'linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url("../../../images/home-bg.jpg")'
+  background:
+    'linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url("../../../images/home-bg.jpg")'
 };
 
 var imgTwo = {
-	background:
-		'linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url("../../../images/home-bg2.jpg")'
+  background:
+    'linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url("../../../images/home-bg2.jpg")'
 };
 
 var imgTree = {
-	background:
-		'linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url("../../../images/home-bg3.jpg")'
+  background:
+    'linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url("../../../images/home-bg3.jpg")'
 };
 
 class Main extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			itemNumber: 1,
-			sliderBtnActiveStyle: sliderBtnActiveStyle,
-			imgSource: imgOne
-		};
+    this.state = {
+      itemNumber: 1,
+      sliderBtnActiveStyle: sliderBtnActiveStyle,
+      imgSource: imgOne
+    };
 
-		this.switchSliderImg = this.switchSliderImg.bind(this);
-	}
+    this.switchSliderImg = this.switchSliderImg.bind(this);
+  }
 
-	componentDidMount() {
-		setTimeout(this.switchSliderImg, 4000);
-	}
+  componentDidMount() {
+    setTimeout(this.switchSliderImg, 4000);
+  }
 
-	switchSliderImg(e) {
-		let itemNumber;
-		if (e) {
-			this.setState({
-				itemNumber: +e.target.dataset.itemNum
-			});
+  switchSliderImg(e) {
+    let itemNumber;
+    if (e) {
+      this.setState({
+        itemNumber: +e.target.dataset.itemNum
+      });
 
-			let target = e.target;
-			itemNumber = +target.dataset.itemNum;
-		} else {
-			itemNumber =
-				this.state.itemNumber === 3
-					? 1 ? this.setState({ itemNumber: 1 }) : ''
-					: ++this.state.itemNumber;
-		}
+      let target = e.target;
+      itemNumber = +target.dataset.itemNum;
+    } else {
+      itemNumber =
+        this.state.itemNumber === 3
+          ? 1 ? this.setState({ itemNumber: 1 }) : ""
+          : ++this.state.itemNumber;
+    }
 
-		switch (itemNumber) {
-			case 1:
-				this.setState({ imgSource: imgOne });
-				break;
+    switch (itemNumber) {
+      case 1:
+        this.setState({ imgSource: imgOne });
+        break;
 
-			case 2:
-				this.setState({ imgSource: imgTwo });
-				break;
+      case 2:
+        this.setState({ imgSource: imgTwo });
+        break;
 
-			case 3:
-				this.setState({ imgSource: imgTree });
-				break;
+      case 3:
+        this.setState({ imgSource: imgTree });
+        break;
 
-			default:
-				this.setState({ imgSource: imgOne });
-				break;
-		}
-		if (!e) setTimeout(this.switchSliderImg, 4000);
-	}
+      default:
+        this.setState({ imgSource: imgOne });
+        break;
+    }
+    if (!e) setTimeout(this.switchSliderImg, 4000);
+  }
 
-	render() {
-		return (
-			<main style={this.state.imgSource} className={styles['main-part']}>
-				<Route exact path="/" component={Home} />
-				<Route exact path="/login" component={LoginContainer} />
+  render() {
+    return (
+      <main style={this.state.imgSource} className={styles["main-part"]}>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={LoginContainer} />
 
-				<div
-					onClick={this.switchSliderImg}
-					className={styles['slider-buttons-container']}
-				>
-					<button
-						style={this.state.itemNumber === 1 ? sliderBtnActiveStyle : {}}
-						type="button"
-						className={`${styles['slider-buttons-item']} ${styles[
-							'slider-buttons-item-active'
-						]}`}
-						data-item-num="1"
-					/>
-					<button
-						style={this.state.itemNumber === 2 ? sliderBtnActiveStyle : {}}
-						type="button"
-						className={styles['slider-buttons-item']}
-						data-item-num="2"
-					/>
-					<button
-						style={this.state.itemNumber === 3 ? sliderBtnActiveStyle : {}}
-						type="button"
-						className={styles['slider-buttons-item']}
-						data-item-num="3"
-					/>
-				</div>
-			</main>
-		);
-	}
+        <div
+          onClick={this.switchSliderImg}
+          className={styles["slider-buttons-container"]}
+        >
+          <button
+            style={this.state.itemNumber === 1 ? sliderBtnActiveStyle : {}}
+            type="button"
+            className={`${styles["slider-buttons-item"]} ${styles[
+              "slider-buttons-item-active"
+            ]}`}
+            data-item-num="1"
+          />
+          <button
+            style={this.state.itemNumber === 2 ? sliderBtnActiveStyle : {}}
+            type="button"
+            className={styles["slider-buttons-item"]}
+            data-item-num="2"
+          />
+          <button
+            style={this.state.itemNumber === 3 ? sliderBtnActiveStyle : {}}
+            type="button"
+            className={styles["slider-buttons-item"]}
+            data-item-num="3"
+          />
+        </div>
+      </main>
+    );
+  }
 }
 
 export default Main;
