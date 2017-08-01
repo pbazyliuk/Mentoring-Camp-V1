98ef9325d6e289f408066ec606a427f89b6e38b1
Pavlo Bazyliuk <pavlo.bazyliuk@eleks.com>
2017-08-01 12:10:39 +0300

    added login component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,23 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styles from "./Home.scss";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Home.scss';
 
 class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1 className={styles["main-heading"]}>
-          Service for developers <br /> to grow with professional mentors
-        </h1>
+	render() {
+		return (
+			<div className={styles['home-container']}>
+				<h1 className={styles['main-heading']}>
+					Service for developers <br /> to grow with professional mentors
+				</h1>
 
-        <Link to="/login">
-          <button className={styles["login-button"]} type="button">
-            Login
-          </button>
-        </Link>
-      </div>
-    );
-  }
+				<Link to="/login">
+					<button className={styles['login-button']} type="button">
+						Login
+					</button>
+				</Link>
+			</div>
+		);
+	}
 }
 
 export default Home;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,7 +6,51 @@ import styles from './Login.scss';
 
 class Login extends React.Component {
 	render() {
-		return <div>Login Component</div>;
+		return (
+			<div className={styles['login-form-container']}>
+				<form className={styles['login-form']} action="">
+					<div className={styles['login-form-name']}>Login Form</div>
+					<label className={styles['login-form-label']} htmlFor="emailField">
+						Email
+					</label>
+					<input
+						className={styles['login-form-input']}
+						type="text"
+						id="emailField"
+						placeholder="Input Email"
+					/>
+					<label className={styles['login-form-label']} htmlFor="passwordField">
+						Password
+					</label>
+					<input
+						className={styles['login-form-input']}
+						type="password"
+						id="passwordField"
+						placeholder="Input Password"
+					/>
+					<div className={styles['social-icons-container']}>
+						<img
+							className={styles['facebook']}
+							alt=""
+							src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgAQMAAABJtOi3AAAAA1BMVEX///+nxBvIAAAAAXRSTlMAQObYZgAAAAxJREFUeNpjYBjcAAAAoAABjvuxtAAAAABJRU5ErkJggg=="
+						/>
+
+						<img
+							className={styles['github-logo']}
+							alt=""
+							src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgAQMAAABJtOi3AAAAA1BMVEX///+nxBvIAAAAAXRSTlMAQObYZgAAAAxJREFUeNpjYBjcAAAAoAABjvuxtAAAAABJRU5ErkJggg=="
+						/>
+						<img
+							className={styles['google-plus-logo-on-black-background']}
+							alt=""
+							src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgAQMAAABJtOi3AAAAA1BMVEX///+nxBvIAAAAAXRSTlMAQObYZgAAAAxJREFUeNpjYBjcAAAAoAABjvuxtAAAAABJRU5ErkJggg=="
+						/>
+					</div>
+
+					<button className={styles['login-form-button']}>Login</button>
+				</form>
+			</div>
+		);
 	}
 }
 
